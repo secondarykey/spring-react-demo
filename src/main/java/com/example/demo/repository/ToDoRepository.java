307096ad0d00f4f8b3873de1309f76dd40c55866
secondarykey <secondarykey@gmail.com>
2021-12-15 10:45:30 +0900

    feat: change access method

--- src/main/java/com/example/demo/repository/ToDoRepository.java
+++ src/main/java/com/example/demo/repository/ToDoRepository.java
@@ -1,37 +1,10 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.model.ToDo;
 
 @Repository
-public class ToDoRepository extends AccessRepository {
-
-	@Autowired(required=true)
-	public ToDoRepository(JdbcTemplate template) {
-		super(template);
-	}
-
-	public List<ToDo> find() {
-		String sql = "SELECT * FROM TODOS";
-		return this.select(ToDo.class,sql);
-	}
-
-	@Transactional
-	public void insert(ToDo todo) {
-		String sql = "INSERT INTO TODOS(VALUE) VALUES (?)";
-		Number num = this.insert(sql,todo.getValue());
-		todo.setId(num.intValue());
-	}
-
-	@Transactional
-	public int delete(ToDo todo) {
-		String sql = "DELETE FROM TODOS WHERE id = ?";
-		return this.update(sql,todo.getId());
-	}
+public interface ToDoRepository extends CrudRepository<ToDo,Integer> {
 }
