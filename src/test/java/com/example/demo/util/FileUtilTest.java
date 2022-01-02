672f1184f0ee8c6ec0dd7bf3735c6a07f46d248f
secondarykey <secondarykey@gmail.com>
2022-01-03 06:06:02 +0900

    fix: add FileUtil

---
+++ src/test/java/com/example/demo/util/FileUtilTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FileUtilTest {
+	
+	@Test
+	void testJoin() {
+		String got = FileUtil.join("test","join");
+		assertEquals(got,"test\\join");
+
+		got = FileUtil.join();
+		assertEquals(got,"");
+	}
+
+}
