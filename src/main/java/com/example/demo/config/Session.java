3c8376c08c1b178b08fdebcad7438426776a77fe
secondarykey <secondarykey@gmail.com>
2022-01-03 05:01:20 +0900

    fix: change the setting method of the default language

--- src/main/java/com/example/demo/config/Session.java
+++ src/main/java/com/example/demo/config/Session.java
@@ -4,30 +4,52 @@ import java.io.Serializable;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.stereotype.Component;
+
+import com.example.demo.util.Util;
 
 @Component
 @ConditionalOnWebApplication
 public class Session implements Serializable {
+	
+	private final static Logger logger = LoggerFactory.getLogger(Session.class);
 
 	private static final long serialVersionUID = 1L;
 
+	public static final String sessionUserKey = "session";
+	public static final String sessionLanguageKey = "language";
+
+	@Autowired
+    @Value("${application.default.language}")
+    private String defaultLanguage;
     @Autowired
-    HttpSession session;
-
-	public Session() {
-	}
-
+    private HttpSession session;
+    /**
+     * 言語の取得
+     * @return 言語コード
+     */
 	public String getLanguage() {
-		Object sess = session.getAttribute("language");
-		if ( sess == null ) {
-			return "ja";
+		Object sessObj = session.getAttribute(sessionLanguageKey);
+		if ( sessObj == null ) {
+			return defaultLanguage;
 		}
-		return (String)sess;
+		return (String)sessObj;
 	}
-	public void setLanguage(String language) {
-		session.setAttribute("language", language);
+
+	/**
+	 * 言語の設定
+	 * @param language
+	 */
+	public void setLanguage(String lang) {
+		if ( Util.isDefault(lang) ) {
+			lang = defaultLanguage;
+		}
+		logger.info("session language {},default {}",lang,defaultLanguage);
+		session.setAttribute(sessionLanguageKey, lang);
 	}
 }
