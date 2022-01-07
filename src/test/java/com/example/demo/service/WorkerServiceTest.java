5d17189177ef816f0aa89ba3a3773b34eb692e70
secondarykey <secondarykey@gmail.com>
2022-01-07 10:11:24 +0900

    test: add workers test

--- src/test/java/com/example/demo/service/WorkerServiceTest.java
+++ src/test/java/com/example/demo/service/WorkerServiceTest.java
@@ -26,5 +26,28 @@ public class WorkerServiceTest {
 		assertNotNull(list);
 		assertEquals(list.size(),2);
 		
+		//TODO Workerはテストを後回し
+		// データ構造があやしい
+	
+		/**
+		OperationResponse opeRes = list.get(0);
+		assertEquals(opeRes.getWorkers(),1);
+		opeRes = list.get(1);
+		assertEquals(opeRes.getWorkers(),1);
+
+		res = service.find(4, "2022-01-07", "ja");
+		list = res.getOperationList();
+		assertNotNull(list);
+		assertEquals(list.size(),3);
+		opeRes = list.get(0);
+		assertEquals(opeRes.getWorkers(),2,"同一作業に２人いること");
+		opeRes = list.get(1);
+		assertEquals(opeRes.getWorkers(),0,"いない作業が表現できること");
+		opeRes = list.get(2);
+		assertEquals(opeRes.getWorkers(),1,"１人の作業もデータ化されていること");
+		*/
+
+		res = service.find(1, "2021-12-31", "ja");
+		assertNull(res,"作業が存在しない場合、Null");
 	}
 }
