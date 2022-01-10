1c68e63fa21aed31da44fa27c7f6557efa58f99c
secondarykey <secondarykey@gmail.com>
2022-01-11 04:17:08 +0900

    feat: moved

--- src/test/java/com/example/demo/config/AuthenticationInterceptorTest.java
+++ src/test/java/com/example/demo/config/AuthenticationInterceptorTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.config;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
