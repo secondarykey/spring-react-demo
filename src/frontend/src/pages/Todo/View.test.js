ab1dfae7db3681c673a6c4e86c305e5a9bc0fede
secondarykey <secondarykey@gmail.com>
2022-01-11 04:22:00 +0900

    test: prepare the foundation

---
+++ src/frontend/src/pages/Todo/View.test.js
@@ -0,0 +1,16 @@
+import { render,screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+
+import Test from "../TestPages"
+import View from "./View"
+
+describe("Todo/View",() => {
+    test("Todo View",() => {
+        act( () => {
+            const container = render(<Test><View /></Test>)
+            screen.debug();
+            expect(container).toMatchSnapshot();
+        })
+
+    })
+})
