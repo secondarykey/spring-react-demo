c28bb48ab3b1db0a0d54a521fb3cd9b2bcd9aebd
secondarykey <secondarykey@gmail.com>
2022-08-21 14:26:41 +0900

    fix: added file upload demo

--- src/frontend/src/pages/Demo/File/Upload.js
+++ src/frontend/src/pages/Demo/File/Upload.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+
+import axios from 'axios';
 
 import {WriteErrorMessage} from '../../../Layout';
 import API from "../../../API"
-import {ShowConfirm} from "../../../Dialog"
 
 class UploadFile extends React.Component {
 
   constructor(props) {
-
     super(props);
     this.file = React.createRef();
     this.upload = this.upload.bind(this);
+
+    this.state = {
+      id:0, filename:""
+    }
   }
   
   componentDidMount() {
@@ -23,18 +25,42 @@ class UploadFile extends React.Component {
   upload() {
 
     var formData = new FormData();
-    console.log(this.file.current);
-    formData.append("file",this.file.current.files[0]);
+    var f = this.file.current.files[0];
+    formData.append("file",f);
+    var name = f.name;
 
     API.put("/api/demo/file/upload",resp => {
 
-      console.log(resp);
+      var id = resp.data.result;
+      this.setState({
+        id : id,
+        filename:name
+      });
 
     },formData).catch((err) => {
       if ( API.isUnknownError(err) ) {
         return;
       }
       WriteErrorMessage(err);
+    });
+  }
+  
+  handleDownload(id) {
+    let url = "/api/demo/file/get/" + id;
+    //window.open("/api/demo/file/get/" + id);
+    axios.get(url, {
+      responseType: 'blob',
+    }).then((res) => {
+      //fileDownload(res.data, filename)
+        const url = window.URL.createObjectURL(new Blob([res.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download",this.state.filename);
+        document.body.appendChild(link);
+        link.click();
+
+        // Clean up and remove the link
+        link.parentNode.removeChild(link);
     });
   }
 
@@ -44,7 +70,18 @@ class UploadFile extends React.Component {
         <Form.Label>Default file input example</Form.Label>
         <Form.Control type="file" ref={this.file}/>
       </Form.Group>
-      <Button onClick={(e) => this.upload()} />
+      <Button onClick={(e) => this.upload()}>Upload</Button>
+
+<br/><br/>
+
+  {this.state.id != 0 &&
+<>
+  <a href="#" onClick={() => this.handleDownload(this.state.id)}>Download1</a>
+  {/** 
+  <a href={"/api/demo/file/get/" + this.state.id} download>Download2</a>
+  */}
+</>
+  }
 	</>);
   }
 }
